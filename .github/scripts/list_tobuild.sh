f2fc0c1678951c5bed479d012d040e02a6e2233c
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-11-16 13:36:11 -0500

    Update list_tobuild.sh

--- .github/scripts/list_tobuild.sh
+++ .github/scripts/list_tobuild.sh
@@ -27,11 +27,11 @@ if [ ! -s tobuild.txt ]; then
                     mkdir -p logs/$(cat runstarttime)/retries
                     if [ $counter -gt 6 ]; then
                         echo 'switch' > lists/self-hosted
+                        rm -rf "logs/$(cat runstarttime)/retries" || true
                     fi
                     cat /tmp/resetpkgs | xargs -i bash -c 'pkgcounter=0; retrypath="logs/$(cat runstarttime)/retries/{}"; if [ -f "$retrypath" ]; then pkgcounter=$(<$retrypath); fi; pkgcounter=$((pkgcounter+1)); mkdir -p $(dirname $retrypath); echo $pkgcounter > "$retrypath"; if [ $pkgcounter -lt 3 ]; then rm {} && rm logs/$(cat runstarttime)/retries_counter || true; fi'
                     git add lists
                     git add logs
-                    git add .github/workflows/run.yaml
                 fi
             fi
             if [ $counter -eq 10 ]; then
